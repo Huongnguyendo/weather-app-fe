1d5a8809cc33668ebbe19c35cd28e7b88824e4b6
huongnguyen307 <huongnguyendo307@gmail.com>
2020-10-09 13:16:45 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,117 @@
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Form,
+  Nav,
+  Navbar,
+  NavDropdown,
+  FormControl,
+  Button,
+  Container,
+  Alert,
+} from "react-bootstrap";
+import WeatherCard from "../src/weatherCard/weatherCard";
+import { css } from "@emotion/core";
+import ClipLoader from "react-spinners/ClipLoader";
+import "./App.css";
+
+function App() {
+  const REACT_APP_BACKEND = process.env.REACT_APP_BACKEND;
+  const [keyword, setKeyword] = useState("");
+  let [weatherData, setWeatherData] = useState();
+  let [loading, setLoading] = useState();
+
+  const override = css`
+    display: block;
+    margin: 0 auto;
+    border-color: red;
+  `;
+
+  const getWeatherData = async (e) => {
+    if (e) e.preventDefault();
+    try {
+      console.log("keyword", keyword);
+      let url = `${REACT_APP_BACKEND}?q=${keyword}`;
+      let response = await fetch(url);
+      if (response.status == 200) {
+        let data = await response.json();
+        console.log("do we get data", response);
+        setWeatherData(data.data);
+      } else throw new Error({ message: "i am new error" });
+    } catch (err) {
+      console.log("err: ", err.message);
+    }
+  };
+
+  const getLocation = () => {
+    navigator.geolocation.getCurrentPosition((post) => {
+      getWeather(post.coords.longitude, post.coords.latitude);
+    });
+  };
+
+  const getWeather = async (longtitude, latitude) => {
+    try {
+      let url = `${REACT_APP_BACKEND}?lon=${longtitude}&lat=${latitude}`;
+      console.log("url: ", url);
+      let response = await fetch(url);
+      console.log("response", response);
+      let data = await response.json();
+      console.log("dataaa", data);
+      setWeatherData(data.data);
+    } catch (e) {
+      alert(e);
+    }
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  // useEffect(() => {
+  //   getWeatherData();
+  // }, []);
+
+  return (
+    <div className="App">
+      <Navbar bg="light" expand="md" className="navbar">
+        <Navbar.Brand href="#home">Weather App</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline onSubmit={(e) => getWeatherData(e)}>
+            <FormControl
+              type="text"
+              placeholder="Search"
+              className="mr-sm-2"
+              onChange={(e) => setKeyword(e.target.value)}
+            />
+            <Button variant="outline-success" type="submit">
+              Search
+            </Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+      <div className="weather-info">
+        <WeatherCard weatherData={weatherData} />
+      </div>
+
+      {/* <Container>{weatherData ? <div>{weatherData.main.temp}</div> : ""}</Container> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/weatherCard/weatherCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import moment from "moment";
+
+const WeatherCard = ({ weatherData }) => {
+  const getCurrentDate = (separator = " ") => {
+    let newDate = new Date();
+
+    return moment(newDate).format("MMM Do YYYY");
+  };
+
+  const getTempC = (tempK) => {
+    return Math.round(tempK - 273.15);
+  };
+
+  if (!weatherData) return <div>Loading...</div>;
+  return (
+    <div className="weather-info-inner">
+      <h2 className="city-name">{weatherData.name}</h2>
+      <h3 className="tempe">{getTempC(weatherData.main.temp)} ºC</h3>
+
+      <div className="weather-info-bottom">
+        <div className="weather-info-detail">
+          <h3 className="humidity">
+            Humidity
+            <br />
+            {weatherData.main.humidity}
+          </h3>
+          <h3 className="description">
+            {weatherData.weather[0].main}
+            <img
+              className="weather-img"
+              src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
+            />
+          </h3>
+        </div>
+        <div className="date">{getCurrentDate()}</div>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherCard;
